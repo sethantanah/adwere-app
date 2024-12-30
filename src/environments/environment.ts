6d9da084e28e39f75a0f5744695b25a6a5c3c3b9
Seth Antanah <55447320+sethantanah@users.noreply.github.com>
2024-12-30 23:16:11 +0000

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,18 +1,1 @@
-export const environment = {
-  production: false,
-  apiUrl2: 'http://localhost:8000/api/v1',
-  apiUrl: 'https://adwere-backend.onrender.com/api/v1',
-  mnotifyApiKey: 'zI6BH4IdqcaRdg3JaGx7KI1iJ',
-  mnotifyEndPoint: 'https://api.mnotify.com/api/sms/quick',
-  senderPhone: '0591971286',
-  firebase: {
-    "projectId": "adwere-d3d14",
-    "appId": "1:4978220684:web:941b984f4fbc323d7465a9",
-    "storageBucket": "adwere-d3d14.appspot.com",
-    "apiKey": "AIzaSyDVtc55IrX-woJz0jov0BoiPaUCXxFYADk",
-    "authDomain": "adwere-d3d14.firebaseapp.com",
-    "messagingSenderId": "4978220684",
-    "measurementId": "G-7JMPPS58JZ"
-  },
-  adminPassword: '12345'
-};
+
